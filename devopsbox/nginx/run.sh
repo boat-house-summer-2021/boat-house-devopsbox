0b26dc6c0fbd37962cbac6cb298a02e11b5e9ff0
xiecf <xiecf_wh@qq.com>
2021-07-22 10:32:25 +0800

    adjust dirs

--- devopsbox/nginx/run.sh
+++
@@ -1,4 +0,0 @@
-docker run  --restart=always  -p 80:80 --name devops_box_nginx -d nginx
-
-# 挂载静态站点和nginx.conf文件
-# docker run  --restart=always  -p 80:80 -v ${PWD}/html:/var/www/html -v ${PWD}/nginx.conf:/etc/nginx/nginx.conf  --name devops_box_nginx -d nginx
